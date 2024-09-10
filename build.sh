4bf5443762f9e7db478954aaf55c37dfff8e8fb5
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-09-10 20:06:44 +0100

    chroe: removed logs, improving code

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ aiken() {
 
 # exit 0
 
-aiken build &> /dev/null
+aiken build -t verbose &> /dev/null
 
 PREPROD_PROTOCOL_BOOT_UTXO="d8799fd8799f5820aadaac3863a0e43da3f4464c2fdccada8d39ffe3a041e01fc5849e8a720e9cf3ff00ff"
 PREPROD_SETTINGS_POLICY_ID="581cb29a6ffd079d50bb4b130bc76a0a13e10eeceb67328bb3e5e46c4eaa"
