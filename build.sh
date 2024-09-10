42f9a4f6235adcd5d4650f8b47ba49916085900c
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-09-10 21:15:44 +0100

    chore: new preprod bootstrap protocol

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ aiken() {
 aiken build -t verbose &> /dev/null
 
 PREPROD_PROTOCOL_BOOT_UTXO="d8799fd8799f58204f79db6c5e935d243340c44ab53f51dc3e6dd1c1a859f22f862482397b6a9281ff00ff"
-PREPROD_SETTINGS_POLICY_ID="581cb29a6ffd079d50bb4b130bc76a0a13e10eeceb67328bb3e5e46c4eaa"
+PREPROD_SETTINGS_POLICY_ID="581c2c1138e039f65a6840462c25c2fcb6b653bb7b07ed9446cf677632f8"
 
 ## Settings
 aiken blueprint apply -v settings.spend $PREPROD_PROTOCOL_BOOT_UTXO 2> /dev/null > tmp
