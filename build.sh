e1fd3cdf0994718e37fb7e270fed9aea7b19b0d1
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-09-12 12:07:37 +0100

    chore: set datum directly in validator

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ aiken() {
 
 # exit 0
 
-# aiken build -t verbose &> /dev/null
-aiken build &> /dev/null
+aiken build -t verbose &> /dev/null
+# aiken build &> /dev/null
+
+## PREPROD ##
 
 PREPROD_PROTOCOL_BOOT_UTXO="d8799fd8799f58204f79db6c5e935d243340c44ab53f51dc3e6dd1c1a859f22f862482397b6a9281ff00ff"
-PREPROD_SETTINGS_POLICY_ID="581c2c1138e039f65a6840462c25c2fcb6b653bb7b07ed9446cf677632f8"
+PREPROD_SETTINGS_POLICY_ID="845a9b6c2341f6214cd00ac42e528440fd0c3707756ad1e1642f6809"
 
 ## Settings
 aiken blueprint apply -v settings.spend $PREPROD_PROTOCOL_BOOT_UTXO 2> /dev/null > tmp
@@ -24,3 +26,19 @@ mv tmp preprod-mint-settings-plutus.json
 ## Recurring payment
 aiken blueprint apply -v automatic_payments.automatic_payments $PREPROD_SETTINGS_POLICY_ID 2> /dev/null > tmp
 mv tmp preprod-automatic-payments-plutus.json
+
+## MAINNET ##
+
+# MAINNET_PROTOCOL_BOOT_UTXO="d8799fd8799f58204f79db6c5e935d243340c44ab53f51dc3e6dd1c1a859f22f862482397b6a9281ff00ff"
+MAINNET_SETTINGS_POLICY_ID="845a9b6c2341f6214cd00ac42e528440fd0c3707756ad1e1642f6809"
+
+## Settings
+# aiken blueprint apply -v settings.spend $PREPROD_PROTOCOL_BOOT_UTXO 2> /dev/null > tmp
+# mv tmp preprod-spend-settings-plutus.json
+
+# aiken blueprint apply -v settings.mint $PREPROD_PROTOCOL_BOOT_UTXO 2> /dev/null > tmp
+# mv tmp preprod-mint-settings-plutus.json
+
+## Recurring payment
+aiken blueprint apply -v automatic_payments.automatic_payments $MAINNET_SETTINGS_POLICY_ID 2> /dev/null > tmp
+mv tmp mainnet-automatic-payments-plutus.json
